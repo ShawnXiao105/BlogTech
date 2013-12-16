e2d5715b8f30859cb5cd0f128a339c473c88978f
cleverxy <yang.xiao105@gmail.com>
2013-12-16 16:33:51 +0800

    Version1.0
    This version add the function of add math equation and fixed the bug
    that image in blog cannot center align

--- myblog/settings.py
+++ myblog/settings.py
@@ -18,7 +18,7 @@ DATABASES = {
 	'NAME': 'datadb',
 	# The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': 'password',
+        'PASSWORD': 'cleverxy',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
